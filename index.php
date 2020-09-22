f28c32b9bae62ef6d16c6fd1a17c01b21e98dcaf
JakubVavru <JakubVavru@users.noreply.github.com>
2020-09-22 15:52:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $eur = filter_input(INPUT_POST, 'eur');
 $czk = 27 ;
 $sub = filter_input(INPUT_POST, 'odeslat');
+$switch =  filter_input(INPUT_POST, 'switch');
 ?>
 
 <!DOCTYPE html>
@@ -14,12 +15,21 @@ $sub = filter_input(INPUT_POST, 'odeslat');
 <body>
 
 <?php
-if (isset($sub)) { ?>
-   Výsledek v Kč: <?= $eur * $czk ?>
-<?php
+if (isset($sub)) {
+    if ($switch == 2) { ?>
+        <?= $eur ?> Eur je <?= $eur * $czk ?> Kč
+        <?php
+    } elseif ($switch == 1) { ?>
+        <?= $eur ?> Kč je <?= $eur / $czk ?> Eur
+        <?php
+    }
+
+
 } else { ?>
     <form action="index.php" method="post">
-Euro:   <input type="text" name="eur" id="eur"> <br>
+Peníze: <input type="text" name="eur" id="eur"> <br>
+        CZK to EUR: <input type="radio" name="switch" value="1" id="switch"><br>
+        EUR to CZK: <input type="radio" name="switch" value="2" id="switch"><br>
         <input type="submit" value="odeslat" name="odeslat">
     </form>
 <?php
